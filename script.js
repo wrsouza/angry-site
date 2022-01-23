975e3c43c20613cc155ca524122667bbaa7f7796
Willian Souza <wrsystem@gmail.com>
2022-01-23 19:53:05 -0300

    complete site with login form modal

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal() {
+  const modal = document.querySelector("#modal");
+  modal.classList.add("show");
+}
+
+function closeModal() {
+  const modal = document.querySelector("#modal");
+  modal.classList.remove("show");
+}
